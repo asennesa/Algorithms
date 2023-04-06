f11488ae420a0ea1da144f94fa49847a9ccc137f
Asen Georgiev <49186568+asennesa@users.noreply.github.com>
2023-04-06 12:12:54 +0300

    Create Breadth-first Search (BFS).py

---
+++ Breadth-first Search (BFS).py
@@ -0,0 +1,27 @@
+from collections import deque
+
+# Define the graph as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+# Define the BFS function
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=' ')
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Call the BFS function with the starting node
+bfs(graph, 'A')
